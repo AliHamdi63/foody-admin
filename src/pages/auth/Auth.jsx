fd4c0ea6ab9ee14536caea52f60b0947cc181367
Ali Hamdi <ali.hamdi.official8@gmail.com>
2022-08-08 21:00:29 +0200

    update

--- src/pages/auth/Auth.jsx
+++ src/pages/auth/Auth.jsx
@@ -1,31 +1,32 @@
 import { useState } from 'react'
 import './Auth.scss'
-import {fetchUser} from '../../redux/reducers/authReducer';
-import {useDispatch, useSelector} from 'react-redux'
+import { fetchUser } from '../../redux/reducers/authReducer';
+import { useDispatch, useSelector } from 'react-redux'
 const Auth = () => {
 
-    let [data,setData] = useState({email:'',password:''});
-    const dispatch = useDispatch()
-    let {isFetching,err} = useSelector(state=>state.auth)
+  let [data, setData] = useState({ email: '', password: '' });
+  const dispatch = useDispatch()
+  let { isFetching, err } = useSelector(state => state.auth)
 
-    const handleChange = (e)=>{
-        setData({...data,[e.target.name]:e.target.value})
-    }
-    
-    const handleSubmit =(e)=>{
-        e.preventDefault();
-        console.log(data)
-        dispatch(fetchUser(data));
-    }
+  const handleChange = (e) => {
+    console.log(data)
+    setData({ ...data, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(data)
+    dispatch(fetchUser(data));
+  }
 
   return (
     <div className='auth'>
       <form onSubmit={handleSubmit}>
         <h1>login</h1>
-        <input type={`email`} name='email' placeholder={`Email..`} onChange={handleChange}/>
-        <input type={`password`} name='password' placeholder={`Password..`} onChange={handleChange}/>
-        <input disabled={isFetching} type={`submit`} value={'Login'}/>
-        {err &&<span>Please try agin...</span>}
+        <input type={`email`} name='email' placeholder={`Email..`} onChange={handleChange} />
+        <input type={`password`} name='password' placeholder={`Password..`} onChange={handleChange} />
+        <input disabled={isFetching} type={`submit`} value={'Login'} />
+        {err && <span>Please try agin...</span>}
       </form>
     </div>
   )
