ffccb31cb0f4eb603c4596853cf86a71d78ff37d
Ali Hamdi <108863727+AliHamdi7@users.noreply.github.com>
2023-01-05 17:13:42 +0200

    modify

--- src/address.js
+++ src/address.js
@@ -1,4 +1,4 @@
-export function getAddress(address){
-    if(!address) return 'empty'
-    return `${address?.BuildingNumber||''} ${address?.street||''} ${address?.street?'st.':''} ${address?.area||''} ${address?.city||''}`
+export function getAddress(address) {
+    if (!address) return 'empty'
+    return `${address?.BuildingNumber || ''} ${address?.street || ''} ${address?.street ? 'st.' : ''} ${address?.area || ''} ${address?.city || ''}`
 }
