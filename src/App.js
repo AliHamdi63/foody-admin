22767800520c21a51014db812b067fa2667a3eb4
Ali Hamdi <aawere1233@gmail.com>
2023-06-20 18:10:13 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -2,41 +2,41 @@ import './App.scss';
 import { useSelector } from "react-redux";
 import Auth from "./pages/auth/Auth";
 import Home from './pages/home/Home';
-import {BrowserRouter,Navigate,Route,Routes} from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import List from './pages/list/List';
 import Single from "./pages/single/Single";
 import New from './pages/new/New'
 import Chat from "./pages/chat/Chat";
 import SingleOrder from "./pages/singleOrder/SingleOrder";
 import UpdateMeal from "./pages/updateMeal/UpdateMeal";
-import VedioChat from './pages/vedioChat/VedioChat';
-import { ContextProvider } from './vedioChatContext';
+// import VedioChat from './pages/vedioChat/VedioChat';
+// import { ContextProvider } from './vedioChatContext';
 function App() {
 
-  let {admin} = useSelector(state=>state.auth);
-  let {value} = useSelector(state=>state.darkmode);
+  let { admin } = useSelector(state => state.auth);
+  let { value } = useSelector(state => state.darkmode);
 
   return (
-    <div className={value?'App darkmode':'App'}>
+    <div className={value ? 'App darkmode' : 'App'}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={admin ? <Home />: <Navigate to={`/login`} />}/>
-          <Route path="/home" element={admin ? <Home />: <Navigate to={`/login`} />}/>
-          <Route path="/login" element={admin ? <Navigate to={`/home`} />: <Auth />}/>
+          <Route path="/" element={admin ? <Home /> : <Navigate to={`/login`} />} />
+          <Route path="/home" element={admin ? <Home /> : <Navigate to={`/login`} />} />
+          <Route path="/login" element={admin ? <Navigate to={`/home`} /> : <Auth />} />
 
-          <Route path="/users" element={admin ?<List />: <Navigate to='/login' /> } />
-          <Route path="/meals" element={admin ?<List />: <Navigate to='/login' /> } />
-          <Route path="/orders" element={admin ?<List />: <Navigate to='/login' /> } />
+          <Route path="/users" element={admin ? <List /> : <Navigate to='/login' />} />
+          <Route path="/meals" element={admin ? <List /> : <Navigate to='/login' />} />
+          <Route path="/orders" element={admin ? <List /> : <Navigate to='/login' />} />
 
-          <Route path="/users/:id" element={admin ?<Single />: <Navigate to='/login' /> } />
-          <Route path="/meals/:id" element={admin ?<UpdateMeal />: <Navigate to='/login' /> } />
-          <Route path="/orders/:id" element={admin ?<SingleOrder />: <Navigate to='/login' /> } />
+          <Route path="/users/:id" element={admin ? <Single /> : <Navigate to='/login' />} />
+          <Route path="/meals/:id" element={admin ? <UpdateMeal /> : <Navigate to='/login' />} />
+          <Route path="/orders/:id" element={admin ? <SingleOrder /> : <Navigate to='/login' />} />
 
-          <Route path="/addUser" element={admin ?<New />: <Navigate to='/login' /> } />
-          <Route path="/addMeal" element={admin ?<New />: <Navigate to='/login' /> } />
-          
-          <Route path="/chat" element={admin ?<Chat />: <Navigate to='/login' /> } />
-          <Route path="/vediochat" element={admin?<ContextProvider><VedioChat /></ContextProvider>:<Navigate to='/login' />} />
+          <Route path="/addUser" element={admin ? <New /> : <Navigate to='/login' />} />
+          <Route path="/addMeal" element={admin ? <New /> : <Navigate to='/login' />} />
+
+          <Route path="/chat" element={admin ? <Chat /> : <Navigate to='/login' />} />
+          {/* <Route path="/vediochat" element={admin?<ContextProvider><VedioChat /></ContextProvider>:<Navigate to='/login' />} /> */}
         </Routes>
       </BrowserRouter>
     </div>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,17 +8,17 @@ import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
 import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import {toggleMode} from '../../redux/reducers/darkmode';
-import VideocamIcon from '@mui/icons-material/Videocam';
+import { toggleMode } from '../../redux/reducers/darkmode';
+// import VideocamIcon from '@mui/icons-material/Videocam';
 
 
 
- const Navbar = () => {
-  const {admin} = useSelector(state=>state.auth);
+const Navbar = () => {
+  const { admin } = useSelector(state => state.auth);
   let imgP = process.env.REACT_APP_SERVER_URL + 'images';
   let dispatch = useDispatch();
-  
-  const toggledark =()=>{
+
+  const toggledark = () => {
     dispatch(toggleMode())
   }
 
@@ -26,44 +26,44 @@ import VideocamIcon from '@mui/icons-material/Videocam';
     <div className='navbar'>
       <div className='left'>
         <div className='searchContainer'>
-          <input className='searchInput' placeholder='search..'/>
-          <SearchOutlinedIcon className='SearchIcon'/>
+          <input className='searchInput' placeholder='search..' />
+          <SearchOutlinedIcon className='SearchIcon' />
         </div>
       </div>
       <div className='right'>
         <div className='item'>
-          <LanguageOutlinedIcon className='icon'/>
+          <LanguageOutlinedIcon className='icon' />
           English
         </div>
         <div className='item'>
           <span onClick={toggledark}>
-          <DarkModeOutlinedIcon className='icon'/>
+            <DarkModeOutlinedIcon className='icon' />
           </span>
         </div>
         <div className='item'>
-          <NotificationsNoneOutlinedIcon className='icon'/>
+          <NotificationsNoneOutlinedIcon className='icon' />
           <div className='counter'>1</div>
         </div>
         <div className='item'>
-          <Link to='/chat' style={{color:'inherit'}}>
-          <ChatBubbleOutlineOutlinedIcon className='icon'/>
+          <Link to='/chat' style={{ color: 'inherit' }}>
+            <ChatBubbleOutlineOutlinedIcon className='icon' />
           </Link>
         </div>
-        <div className='item'>
+        {/* <div className='item'>
         <Link to='/vediochat' style={{color:'inherit'}}>
           <VideocamIcon className='icon'/>
           </Link>
-        </div>
+        </div> */}
         <div className='item'>
-        <Link to='/home' style={{color:'inherit'}}>
-          <ListOutlinedIcon className='icon'/>
+          <Link to='/home' style={{ color: 'inherit' }}>
+            <ListOutlinedIcon className='icon' />
           </Link>
         </div>
         <div className='item'>
-          <img src={`${admin.image}`} className='avatar'/>
+          <img src={`${admin.image}`} className='avatar' />
         </div>
         <div className='item'>
-          <SettingsOutlinedIcon className='icon'/>
+          <SettingsOutlinedIcon className='icon' />
         </div>
       </div>
     </div>
